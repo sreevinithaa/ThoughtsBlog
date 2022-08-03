bbc2727e7306e6a988ffc1bf1b8546bd91df538f
Vinitha Gowtheepan <sreevinithaa@gmail.com>
2022-08-03 16:53:47 +0800

    update user service client side

--- client/src/app/services/user-service.service.ts
+++ client/src/app/services/user-service.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../User';
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  }),
+};
 @Injectable({
   providedIn: 'root'
 })
 export class UserServiceService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:3001/api/users';
+
+  constructor(private http: HttpClient) {}
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.apiUrl);
+  }
+  getSingleUser(id: string): Observable<User> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.get<User>(url);
+  }
+  loginUser(user: User): Observable<User> {
+    const url = `${this.apiUrl}/login`;
+    return this.http.post<User>(url, user, httpOptions);
+  }
+  addUser(user: User): Observable<User> {
+    return this.http.post<User>(this.apiUrl, user, httpOptions);
+  }
 }
